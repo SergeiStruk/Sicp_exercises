05629dd51528bb173ca8a371bb4fbb8edafedb59
s <s@s.(none)>
2013-03-09 00:45:35 +0200

    Fxi 1-08.

--- 1-08.rkt
+++ 1-08.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require rackunit)
 
+(define eps 0.0001)
+
 (define (square x)
     (* x x))
 
@@ -13,20 +15,18 @@
       (* 2 guess))
    3))
 
-(define (good-enough? guess x) 
-    (< (abs (- (cube guess) x)) 0.001)) 
+(define (good-enough? guess prev_guess)
+  (if (< (abs guess) eps)
+      #t
+      (< (/ (abs (- prev_guess guess)) prev_guess) eps)))
 
+(define (cube-root-iter guess prev_guess x)
+  (if (good-enough? guess prev_guess)
+      guess
+      (cube-root-iter (improve guess x) guess x)))
 
-(define (cube-root-iter guess x) 
-    (if (good-enough? guess x) 
-        guess 
-        (cube-root-iter (improve guess x) 
-                        x))) 
 (define (cube-root x) 
-    (cube-root-iter 1.0 x)) 
+    (cube-root-iter 1.0 0.0 x)) 
 
-
-
-(check < (- (cube-root 27) 3) 0.001)
-
-
+(check < (- (cube-root 0) 3) eps)
+(check < (- (cube-root 27) 3) eps)
